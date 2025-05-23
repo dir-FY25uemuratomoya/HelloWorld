daaacd7e76f808594fed16d0586ed7d915f987dc
dir-FY25uemuratomoya <tomoya.uemura.g704@dirbato.co.jp>
2025-05-23 11:18:10 +0900

    Gitの練習です

--- HelloWorld/src/first/Hello.java
+++ HelloWorld/src/first/Hello.java
@@ -3,7 +3,8 @@ package first;
 public class Hello {
 
     public static void main(String[] args) {
-   System.out.print("Hello,World!");
+   System.out.println("Hello,World!");
+   System.out.println("こんにちは、世界!");
 
     }
 
